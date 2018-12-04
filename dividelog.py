660b6fa75f3fba0a5220f1111415efb0a566888b
Sui Zhiyuan <suizhiyuan024135@gmail.com>
2018-12-04 20:28:26 +0800

    m5

--- dividelog.py
+++ dividelog.py
@@ -1,9 +1,9 @@
-with open('log2018_12_03_10_14_19.txt') as myfile:
+with open('log2018_12_04_20_17_58.txt') as myfile:
     index = 0
     while True:
         index += 1
         try:
-            with open('log2018_12_03_10_14_19_part_%03d.txt' % index, 'w') as f:
+            with open('log2018_12_04_20_17_58_part_%03d.txt' % index, 'w') as f:
                 for _ in xrange(100000):
                     f.write(myfile.next())
         except StopIteration:
